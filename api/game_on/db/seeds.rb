8198948c4957f4d3233a6df89d18df38b5cd96a4
Talal Talhouk <email": null>
2016-06-04 22:10:39 -0700

    update seeds

--- api/game_on/db/seeds.rb
+++ api/game_on/db/seeds.rb
@@ -1,27 +1,27 @@
 
 
-Player.create(
-  name: "bob",
-  password: "123",
-  email: "a@b.com",
-  phone:"1234567890",
-  zip_code: "94087",
-  city: "sunnyvale")
+# Player.create(
+#   name: "bob",
+#   password: "123",
+#   email: "a@b.com",
+#   phone:"1234567890",
+#   zip_code: "94087",
+#   city: "sunnyvale")
 
-Player.create(
-  name: "jim",
-  password: "123",
-  email: "c@d.com",
-  phone:"5555555555",
-  zip_code: "94087",
-  city: "sunnyvale")
+# Player.create(
+#   name: "jim",
+#   password: "123",
+#   email: "c@d.com",
+#   phone:"5555555555",
+#   zip_code: "94087",
+#   city: "sunnyvale")
 
 20.times do
   Player.create(
   name: Faker::Name.first_name,
   password: "123",
   email: Faker::Internet.email,
-  phone:"(#{Faker::PhoneNumber.area_code})"+ Faker::PhoneNumber.exchange_code + "-" + Faker::PhoneNumber.subscriber_number,
+  phone:"(#{Faker::PhoneNumber.area_code}) #{Faker::PhoneNumber.exchange_code} - #{Faker::PhoneNumber.subscriber_number}",
   zip_code: [94101, 94105, 94110, 94115].sample,
   city: Faker::Address.city
   )
@@ -52,19 +52,17 @@ end
 end
 
 5.times do
-Player.all.each do |player|
-  player.players_teams.create(
-    team_id: Team.all.sample.id
-    )
+  Player.all.each do |player|
+    player.players_teams.create(
+      team_id: Team.all.sample.id
+      )
+  end
 end
 
 Game.all.each do |game|
-  random_id = rand(1..(Team.all.count - 1))
+  random_id = rand(1..(Team.count - 1))
   game.games_teams.create(
     home_team_id: random_id,
-    away_team_id: random_id + 1,
-    )
+    away_team_id: random_id + 1
+  )
 end
-
-
-
