fcea7150d13421dbb5982af0a9d1ecc171d86acd
Bill <email": null>
2016-06-09 12:13:28 -0700

    Fix positioning on profile team

--- components/TeamProfile.js
+++ components/TeamProfile.js
@@ -83,6 +83,8 @@ class TeamProfile extends Component {
   render() {
     var roster = this.state.userInfo.roster;
     if (this.state.userInfo.team && !this.state.userInfo.isManager) {
+      this.state.userInfo.team.name = this.state.userInfo.team.name[0].toUpperCase() + this.state.userInfo.team.name.substring(1);
+
     return (
 
       <View style={[styles.container, {backgroundColor: 'rgba(0,0,0,0.4)'}]}>
@@ -92,9 +94,9 @@ class TeamProfile extends Component {
           style={[styles.backgroundImage, {resizeMode: "cover"}]}>
           <View style={styles.teamProfileBox}>
 
-            <Text style={styles.h1}>{this.state.userInfo.team.name}</Text>
+            <Text style={[styles.h1, {fontSize: 60}]}>{this.state.userInfo.team.name}</Text>
             <Text style={[styles.h2, {color: "orange"}]}>{this.state.userInfo.team.city}, {this.state.userInfo.team.zip_code}</Text>
-            <Text style={styles.h3}>Manager: {this.state.userInfo.manager.name}</Text>
+            <Text style={[styles.h3, {color: "white", fontWeight: "bold"}]}>Manager: <Text style={{fontWeight: "normal"}}>{this.state.userInfo.manager.name}</Text></Text>
             <Text style={[styles.h4, {fontWeight: "bold", color: "white"}]}>Roster: </Text>
             <ListView
               dataSource={this.state.dataSource}
@@ -104,33 +106,35 @@ class TeamProfile extends Component {
 
       </View>
   )} else if (this.state.userInfo.team && this.state.userInfo.isManager) {
+    this.state.userInfo.team.name = this.state.userInfo.team.name[0].toUpperCase() + this.state.userInfo.team.name.substring(1);
+
     return (
 
   <View style={styles.container}>
 
   <Image
   source={require('../imgs/burning_basketball_desaturate.jpg')}
-  style={[styles.backgroundImage, {resizeMode: "cover", flex: 1, paddingTop: 0}]}>
-    <View style={[{flexDirection: "row"}]}>
-      <View style={{flex: 1}}>
-        <TouchableHighlight style={[styles.button,styles.teamProfileButtons, {backgroundColor: "blue"}]} onPress={this.goToScheduleGame.bind(this)}>
-          <Text style={styles.buttonText}>Make Game</Text>
-        </TouchableHighlight>
-      </View>
-      <View style={{flex: 1}}>
-        <TouchableHighlight style={[styles.button,styles.teamProfileButtons, {backgroundColor: "orange"}]} onPress={this.goToPendingGame.bind(this)}>
-          <Text style={styles.buttonText}>Play Games</Text>
-        </TouchableHighlight>
-      </View>
-    </View>
-      <View style={styles.teamProfileBox}>
-      <Text style={styles.h1}>{this.state.userInfo.team.name}</Text>
+  style={[styles.backgroundImage, {resizeMode: "cover", flex: 1}]}>
+      <View style={[styles.teamProfileBox]}>
+      <Text style={[styles.h1, {fontSize: 60}]}>{this.state.userInfo.team.name}</Text>
       <Text style={[styles.h2, {color: "orange"}]}>{this.state.userInfo.team.city}, {this.state.userInfo.team.zip_code}</Text>
-      <Text style={styles.h3}>Manager: {this.state.userInfo.manager.name}</Text>
+      <Text style={[styles.h3, {color: "white", fontWeight: "bold"}]}>Manager: <Text style={{fontWeight: "normal"}}>{this.state.userInfo.manager.name}</Text></Text>
       <Text style={[styles.h4, {fontWeight: "bold", color: "white"}]}>Roster: </Text>
     <ListView
       dataSource={this.state.dataSource}
       renderRow={this.renderRoster.bind(this)} />
+    </View>
+    <View style={[{flexDirection: "row"}]}>
+    <View style={{flex: 1}}>
+    <TouchableHighlight style={[styles.button,styles.teamProfileButtons, {backgroundColor: "blue", marginTop: 0, marginBottom: 0}]} onPress={this.goToScheduleGame.bind(this)}>
+    <Text style={styles.buttonText}>Make Game</Text>
+    </TouchableHighlight>
+    </View>
+    <View style={{flex: 1}}>
+    <TouchableHighlight style={[styles.button,styles.teamProfileButtons, {backgroundColor: "orange", marginTop: 0, marginBottom: 0}]} onPress={this.goToPendingGame.bind(this)}>
+    <Text style={styles.buttonText}>Play Games</Text>
+    </TouchableHighlight>
+    </View>
     </View>
     </Image>
   </View>
